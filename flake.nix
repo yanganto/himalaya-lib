3fcfa5f5b79cac2462219ed036c76cb9cc59bf86
Clément DOUIN <clement.douin@posteo.net>
2022-07-03 13:29:00 +0200

    simplify nix config

--- flake.nix
+++ flake.nix
@@ -4,42 +4,22 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    naersk.url = "github:nix-community/naersk";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, naersk, ... }:
+  outputs = { self, nixpkgs, utils, rust-overlay, ... }:
     utils.lib.eachDefaultSystem
       (system:
         let
-          name = "himalaya-lib";
           overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs { inherit system overlays; };
         in
-        rec {
-          # nix build
-          defaultPackage = packages.${name};
-          packages = {
-            ${name} = naersk.lib.${system}.buildPackage {
-              pname = name;
-              root = ./.;
-              nativeBuildInputs = with pkgs; [ openssl.dev pkgconfig ];
-            };
-          };
-
-          # nix run
-          defaultApp = apps.${name};
-          apps.${name} = utils.lib.mkApp {
-            inherit name;
-            drv = packages.${name};
-          };
-
+        {
           # nix develop
           devShell = pkgs.mkShell {
-            inputsFrom = builtins.attrValues self.packages.${system};
             nativeBuildInputs = with pkgs; [
               # Nix LSP + formatter
               rnix-lsp
